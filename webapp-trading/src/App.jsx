23b1ededb84722348df2924369a7c1c4798648a9
phwyfve <74145692+phwyfve@users.noreply.github.com>
2026-02-07 19:44:00 +0100

    webapp root paths

--- webapp-trading/src/App.jsx
+++ webapp-trading/src/App.jsx
@@ -7,7 +7,7 @@ import './App.css'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/webtrading">
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
